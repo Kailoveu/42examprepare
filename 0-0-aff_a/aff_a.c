1a38311ec016debd6e5e041d52051e8b1d4118d6
Cankai Zheng <cazheng@e1z1r2p30.42.us.org>
2018-06-03 23:45:40 -0700

    00 -01

---
+++ 0-0-aff_a/aff_a.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   aff_a.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/13 21:38:04 by fwuensch          #+#    #+#             */
+/*   Updated: 2017/07/13 21:41:27 by fwuensch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+char	ft_aff_a(char *str)
+{
+	int		i;
+
+	i = 0;
+	while(str[i] != '\0')
+	{
+		if (str[i] == 'a')
+		{
+			ft_putchar('a');
+			ft_putchar('\n');
+			return (*str);
+		}
+		i++;
+	}
+	ft_putchar('\n');
+	return (*str);
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		ft_aff_a(argv[1]);
+	}
+	else
+	{
+		ft_putchar('a');
+		ft_putchar('\n');
+	}
+	return (0);
+}
